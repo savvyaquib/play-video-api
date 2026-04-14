95634e890f34c4123c6988cb1def91fe34c076ce
savvyaquib <itsmohammadaquib@gmail.com>
2026-04-14 07:57:32 +0530

    add some changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -138,11 +138,13 @@ const loginUser = asyncHandler(async (req, res) => {
 
 
 const logoutUser = asyncHandler(async (req, res) => {
-    await User.findByIdAndUpdate(req.user._id, {
-        $set: {
-            refreshToken: undefined
-        }
-    },
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $unset: {
+                refreshToken: 1 // this removes the field from document
+            }
+        },
         {
             new: true
         }
