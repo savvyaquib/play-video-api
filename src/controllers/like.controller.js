af08845c0cb819fdb284a880a2461d59c6039484
savvyaquib <itsmohammadaquib@gmail.com>
2026-04-26 06:02:38 +0530

    add toggleVideoLike controller

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -7,6 +7,30 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: toggle like on video
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid videoId")
+    }
+
+    const likedVideo = await Like.aggregate([
+        {
+            $match: {
+                video: new mongoose.Types.ObjectId(videoId),
+                likedBy: new mongoose.Types.ObjectId(req.user._id)
+            }
+        }
+    ])
+
+    if (likedVideo.length > 0) {
+        await Like.deleteOne({ _id: likedVideo[0]._id })
+        return res.status(200).json(new ApiResponse(200, { liked: false }, "Video unliked successfully"))
+
+    } else {
+        await Like.create({
+            video: videoId,
+            likedBy: req.user._id
+        })
+        return res.status(200).json(new ApiResponse(200, { liked: true }, "Video liked successfully"))
+    }
 
 })
 
